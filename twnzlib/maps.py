e1e80615bf107905dbd68f4da9e92a5e1dc7f9b0
Twnz <tawan.tpptnk@gmail.com>
2023-10-11 22:33:24 +0100

    Add image map to binary 2D array map conversion

---
+++ twnzlib/maps.py
@@ -0,0 +1,38 @@
+from PIL import Image
+import numpy as np
+from PIL.Image import Resampling
+
+
+def image_to_binary_array(image_path):
+    # Open the image using Pillow
+    image = Image.open(image_path)
+
+    # Downsample the image by 2 times
+    image = image.resize((image.width // 2, image.height // 2), Resampling.LANCZOS)
+
+    # Convert the image to grayscale
+    image = image.convert('L')
+
+    # Convert the image to a NumPy array
+    image_array = np.array(image)
+
+    # Threshold the image to create a binary array
+    threshold = 128  # You can adjust the threshold as needed
+    binary_array = (image_array > threshold).astype(int)
+
+    return binary_array
+
+
+if __name__ == "__main__":
+    # Specify the path to your input image
+    input_image_path = '/Users/twnz/Documents/Repos/twnz-nosty/src/hideout5.png'
+
+    # Convert the image to a binary array
+    binary_array = image_to_binary_array(input_image_path)
+
+    # Print the binary array
+    # print(binary_array)
+    b = [ "".join([str(i) for i in row]).replace("0", "0 ").replace("1", "  ") for row in binary_array.tolist()]
+
+    print('\n'.join(b))
+    print(binary_array.shape)
