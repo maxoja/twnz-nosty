40a32c998add0ce08caf17624e45d2441ad4e585
Twnz <tawan.tpptnk@gmail.com>
2023-10-15 16:05:12 +0100

    Add twnzpb lib with pb flows and login ui

---
+++ twnzpb/__init__.py
@@ -0,0 +1,2 @@
+from . import flows
+from . import ui
---
+++ twnzpb/flows.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+from pocketbase import PocketBase  # Client also works the same
+from pocketbase.services.record_service import RecordAuthResponse
+from pocketbase.utils import ClientResponseError
+
+COL_USERS = 'users'
+COL_CREDITS = 'credits'
+
+
+def __users(pb: PocketBase):
+    return pb.collection(COL_USERS)
+
+
+def __credits(pb: PocketBase):
+    return pb.collection(COL_CREDITS)
+
+
+def verify_email(pb: PocketBase, email: str):
+    __users(pb).requestVerification(email)
+
+
+def reset_password(pb: PocketBase, email: str):
+    __users(pb).requestPasswordReset(email)
+
+
+def get_credits(pb: PocketBase) -> int:
+    print(pb.auth_store.model.id)
+    result = __credits(pb).get_one(
+        pb.auth_store.model.id
+    ).__dict__
+    return result['amount_gbp']
+
+
+def login(pb: PocketBase, email: str, password: str) -> Optional[RecordAuthResponse]:
+    try:
+        return __users(pb).auth_with_password(email, password)
+    except ClientResponseError:
+        return None
