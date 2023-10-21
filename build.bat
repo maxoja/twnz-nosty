64939d879ec19c369698b180983f82e4339dec10
Tawan T <tawan.tpptnk@gmail.com>
2023-10-21 12:49:50 +0100

    Re-Resolves #27 Nest up zip content (#38)

--- build.bat
+++ build.bat
@@ -16,16 +16,16 @@ mkdir .\dist\src
 mkdir .\dist-archive
 
 :: Copy files from src to dist/src
-xcopy .\src\* .\dist\src\ /E /Y
+mkdir .\temp\nosty-bot\src
+xcopy .\src\* .\temp\nosty-bot\src\ /E /Y
+xcopy .\dist\* .\temp\nosty-bot\ /E /Y
 
 :: Get the current date and time in a format like "yyyyMMdd-HHmmss"
 for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set datetime=%%a
 set "DATE_STR=%datetime:~0,8%-%datetime:~8,6%"
 
 :: Zip the dist folder with the timestamp in the filename
-:: powershell -Command "Compress-Archive -Path .\dist -DestinationPath .\dist\mac-%DATE_STR%.zip"
-
 set "destinationPath=.\dist-archive\mac-!DATE_STR!.zip"
-powershell.exe -Command "Compress-Archive -Path '.\dist\*' -DestinationPath '!destinationPath!'"
+powershell.exe -Command "Compress-Archive -Path '.\temp\*' -DestinationPath '!destinationPath!'"
 
 endlocal
